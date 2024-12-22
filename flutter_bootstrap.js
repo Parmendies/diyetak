bd69a6cb4fa358e55612602939a7dd1588316048
Parmendies <bunakdeniz16@gmail.com>
2024-12-22 13:02:14 +0300

    header

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "158166296"
+    serviceWorkerVersion: "3713103274"
   }
 });
